46d3fb1bf31af24cef566f026dacd1d8fa46f203
Joseph Breitner <46498860+joseph-breitner@users.noreply.github.com>
2019-03-24 19:00:31 -0400

    The "Session" class for the UI
    -Still very much in progress but this is what I have so far
    -No idea if it works yet, I'm uploading it here to get it on the Pi so I can give what I have a try

---
+++ session.py
@@ -0,0 +1,139 @@
+from frame import Frame
+from algocfop import AlgoCFOP
+from cube import Cube
+from image import Image
+import time
+import numpy as np
+
+class Session:
+
+    def __init__(self):
+        self.frame = Frame()
+
+    def prompt(self):
+        frame = self.frame
+
+        print('Press <1> to open the claws')
+        print('Press <2> to close the claws')
+        print('Press <3> to start')
+        print('Press <4> to pause')
+        print('Press <5> to randomize cube')
+        option = input('Please select an option from the list above and hit enter: ')
+
+        if option == '1':
+            frame.openClaws()
+        
+        elif option == '2':
+            frame.closeClaws()
+
+        elif option == '3':
+            frame.closeClaws()
+            cube = self.makeCube()
+            algo = AlgoCFOP(cube)
+            algo.solve()
+            self.solveCube(algo.movelist)
+
+        elif option == '4':
+            pass
+        
+        elif option == '5':
+            pass
+        
+        else:
+            print('ERROR: Invalid entry')
+    
+    def makeCube(self):
+        frame = self.frame
+        img1 = Image()
+        img2 = Image()
+        faces = np.empty(6)
+
+        flip = 'X'
+        for i in faces:
+            frame.openClawsFB()
+            img1 = img1.capture()
+            colorArray = img1.makeFace('A')     
+            frame.closeClawsFB()
+            
+            frame.openClawsLR()
+            img2 = img2.capture()
+            f = img2.makeFace('B', colorArray)
+            frame.closeClawsLR()
+            faces[i] = f
+
+            if flip == 'X':
+                frame.rotate90(flip, inverse=True)
+                flip = 'Z'
+            elif flip == 'Z':
+                frame.rotate90(flip)
+                flip == 'X' 
+
+        faces[2] = np.rot90(faces[2], 3)
+        faces[3] = np.rot90(faces[3], 3)
+        faces[4] = np.rot90(faces[4], 1)
+        cube = Cube(faces[0],faces[1],faces[2],faces[3],faces[4],faces[5])
+
+        return cube
+
+    def solveCube(self, movelist):
+        frame = self.frame
+
+        for i in movelist:
+            if movelist[i] == 'X':
+                frame.rotate90('X')
+            elif movelist[i] == 'Xi':
+                frame.rotate90('X', inverse=True)
+            elif movelist[i] == 'Z':
+                frame.rotate90('Z')
+            elif movelist[i] == 'Zi':
+                frame.rotate90('Z', inverse=True)
+            elif movelist[i] == 'Y':
+                frame.rotate90('Y')
+            elif movelist[i] == 'Yi':
+                frame.rotate90('Y', inverse=True)
+            elif movelist[i] == '2X':
+                frame.rotate180('X')
+            elif movelist[i] == '2Y':
+                frame.rotate180('Y')
+            elif movelist[i] == '2Z':
+                frame.rotate180('Z')
+            elif movelist[i] == 'F':
+                frame.turn90('F')
+            elif movelist[i] == 'Fi':
+                frame.turn90('F', inverse=True)
+            elif movelist[i] == 'B':
+                frame.turn90('B')
+            elif movelist[i] == 'Bi':
+                frame.turn90('B', inverse=True)
+            elif movelist[i] == 'L':
+                frame.turn90('L')
+            elif movelist[i] == 'Li':
+                frame.turn90('L', inverse=True)
+            elif movelist[i] == 'R':
+                frame.turn90('R')
+            elif movelist[i] == 'Ri':
+                frame.turn90('R', inverse=True)
+            elif movelist[i] == 'U':
+                frame.turn90('U')
+            elif movelist[i] == 'Ui':
+                frame.turn90('U', inverse=True)
+            elif movelist[i] == 'D':
+                frame.turn90('D')
+            elif movelist[i] == 'Di':
+                frame.turn90('D', inverse=True)
+            elif movelist[i] == '2F':
+                frame.turn180('F')
+            elif movelist[i] == '2B':
+                frame.turn180('B')
+            elif movelist[i] == '2L':
+                frame.turn180('L')
+            elif movelist[i] == '2R':
+                frame.turn180('R')
+            elif movelist[i] == '2U':
+                frame.turn180('U')
+            elif movelist[i] == '2D':
+                frame.turn180('D')
+            else:
+                pass
+
+        return
