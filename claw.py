d87b4534103106d9b17d9a8ae586bc34bdabeaec
Joseph Breitner <46498860+joseph-breitner@users.noreply.github.com>
2019-03-26 18:37:19 -0400

    Added PWM config

--- claw.py
+++ claw.py
@@ -7,6 +7,14 @@ class Claw:
 		self.clawID = clawID
 		self.armID = armID
 		self.kit = ServoKit(channels=16)
+		
+
+		self.kit.servo[1].set_pulse_width_range(700, 2300)
+		self.kit.servo[3].set_pulse_width_range(675, 2355)
+		self.kit.servo[5].set_pulse_width_range(750, 2350)
+		self.kit.servo[7].set_pulse_width_range(750, 2450)
+
+				
 		# self.position
 
 		# initialize claw to 90 deg (0 deg position)
