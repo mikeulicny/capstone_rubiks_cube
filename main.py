ed69d38a82fa60c11522cf53ebf27ae9410723b4
Joseph Breitner <46498860+joseph-breitner@users.noreply.github.com>
2019-04-29 19:36:58 -0400

    added comments

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+#--------------------------------------------------------------
+# ECE 4600 Capstone - Winter 2019
+# Wayne State University
+# Project: Rubik's Cube Solving Robot
+# Primary author: Joseph Breitner
+# Additional team members: Michael Ulicny, Joseph VanBuhler
+#
+# This is the main program to run the Rubik's Cube Solving
+# Robot. When opened it creates a new Session and prompts the
+# user in a while loop until the session is ended. Then the 
+# program is terminated.
+#--------------------------------------------------------------
+
 from session import Session
 
-session = Session()
+# Start new session
+session = Session()	
 
-X = True
+# While loop to keep session open 
+X = True				
 while X:
-	session.printOptions()
-	X = session.prompt()
+	session.printOptions()	# print session options
+	X = session.prompt()	# test to resume / end session
 
